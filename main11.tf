61d2426c1cf2bd544ee3c51cf55b7f2862074003
ashish <ashish222148@gmail.com>
2025-10-03 22:32:08 +0530

    very beautiful dynamic block use

---
+++ main11.tf
@@ -0,0 +1,24 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3389]
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
