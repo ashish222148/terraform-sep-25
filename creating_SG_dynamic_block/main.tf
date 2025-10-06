1ab141f5e4b43ba579afe2e62af8ee60fb78d395
ashish <ashish222148@gmail.com>
2025-10-06 18:28:06 +0530

    best way to create security group

---
+++ creating_SG_dynamic_block/main.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "ailiya-SG" {
+  name        = "Ailiya-SG"
+  description = "this is the security group for ailiya"
+
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value.from_port
+      to_port     = try(port.value.to_port, port.value.from_port)
+      protocol    = "tcp"
+      cidr_blocks = port.value.cidr
+    }
+  }
+}
---
+++ creating_SG_dynamic_block/terraform.tfvars
@@ -0,0 +1,14 @@
+ports = [
+  {
+    from_port = 22
+    cidr      = ["0.0.0.0/0"]
+  },
+  {
+    from_port = 80
+    cidr      = ["0.0.0.0/0"]
+  },
+  {
+    from_port = 443
+    cidr      = ["0.0.0.0/0"]
+  }
+]
